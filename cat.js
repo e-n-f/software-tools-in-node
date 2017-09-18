7976ed90ed3fb0c5eecb8314f3b9869f8eca3b54
Eric Fischer <enf@pobox.com>
2017-09-18 16:37:09 -0700

    It concatenates now

--- cat.js
+++ cat.js
@@ -1,11 +1,25 @@
+'use strict';
+
 const fse = require('fs-extra');
 
-async function main() {
+async function cat(fd) {
 	var b = Buffer.alloc(2000);
 
 	var n;
-	while ((n = await fse.read(0, b, 0, b.length)).bytesRead > 0) {
+	while ((n = await fse.read(fd, b, 0, b.length)).bytesRead > 0) {
 		await fse.write(1, b, 0, n.bytesRead);
+	}
+}
+
+async function main() {
+	if (process.argv.length <= 2) {
+		await cat(0);
+	} else {
+		for (var i = 2; i < process.argv.length; i++) {
+			var fd = await fse.open(process.argv[i], 'r');
+			await cat(fd);
+			await fse.close(fd);
+		}
 	}
 }
 
