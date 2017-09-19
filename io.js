e7653c2a1b7fee133db6403af0f749c01ed6219e
Eric Fischer <enf@pobox.com>
2017-09-19 16:47:22 -0700

    Doesn't work

--- io.js
+++ io.js
@@ -18,27 +18,84 @@ exports.write = async function(fd, buf, off, len) {
 	return (await fse.write(fd, buf, off, len)).bytesWritten;
 };
 
-module.exports.FILE = function(thefd) {
-	this.fd = thefd,
+exports.FILE = function(fd) {
+	this.buffer = Buffer.alloc(512);
+	this.base = 0;
+	this.ptr = 0;
+	this.cnt = this.buffer.length;
+	this.file = fd;
+
+	this.flsbuf = async function(c) {
+		let rn = this.ptr - this.base;
+		let n = rn;
+
+		if (n > 0) {
+			n = await exports.write(this.file, this.buffer, this.base, n);
+			this.ptr = this.base;
+		}
+
+		this.cnt = this.buffer.length - 1;
+		this.buffer[this.base++] = c;
+		this.ptr = base;
+
+		if (n != rn) {
+			throw new Exception();
+		}
+
+		return c;
+	};
+
+	this.filbuf = async function() {
+		this.ptr = this.base;
+		this.cnt = await exports.read(this.file, this.buffer, this.ptr, this.buffer.length);
+		if (this.cnt < 0) {
+			this.cnt = 0;
+			return exports.EOF;
+		}
+
+		return this.buffer[this.ptr++];
+	};
+
+	this.putc = async function(c) {
+		if (this.cnt-- >= 0) {
+			this.buffer[this.ptr++] = c;
+			return c;
+		} else {
+			return await this.flsbuf(c);
+		}
+	};
+
+	this.getc = async function() {
+		if (this.cnt-- >= 0) {
+			return this.buffer[this.ptr++];
+		} else {
+			return await this.filbuf();
+		}
+	}
 
 	this.fread = async function(buf, off, len) {
-		return await module.exports.read(this.fd, buf, off, len);
+		for (let i = off; i < off + len; i++) {
+			buf[i] = await this.getc();
+		}
 	};
 
 	this.fwrite = async function(buf, off, len) {
-		return await module.exports.write(this.fd, buf, off, len);
+		for (let i = off; i < off + len; i++) {
+			await this.putc(buf[i]);
+		}
 	};
 
 	this.fclose = async function() {
-		return await module.exports.close(this.fd);
+		return await exports.close(fd);
 	};
 };
 
-module.exports.stdin = new module.exports.FILE(0);
-module.exports.stdout = new module.exports.FILE(1);
-module.exports.stderr = new module.exports.FILE(2);
+exports.stdin = new exports.FILE(0);
+exports.stdout = new exports.FILE(1);
+exports.stderr = new exports.FILE(2);
+exports.EOF = -1;
 
-module.exports.fopen = async function(name, mode) {
-	var fd = await module.exports.open(name, mode);
-	return new module.exports.FILE(fd);
+exports.fopen = async function(name, mode) {
+	var fd = await exports.open(name, mode);
+	return new exports.FILE(fd);
 }
