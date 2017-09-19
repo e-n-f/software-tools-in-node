8001f46045602a98dcce8a09fea4d1850bf81086
Eric Fischer <enf@pobox.com>
2017-09-19 12:50:08 -0700

    I don't know why I thought I had to say modules

--- io.js
+++ io.js
@@ -2,18 +2,18 @@
 
 const fse = require('fs-extra');
 
-module.exports.open = function(name, mode) {
+exports.open = function(name, mode) {
 	return fse.open(name, mode);
 };
 
-module.exports.close = function(fd) {
+exports.close = function(fd) {
 	return fse.close(fd);
 };
 
-module.exports.read = async function(fd, buf, off, len) {
+exports.read = async function(fd, buf, off, len) {
 	return (await fse.read(fd, buf, off, len)).bytesRead;
 };
 
-module.exports.write = async function(fd, buf, off, len) {
+exports.write = async function(fd, buf, off, len) {
 	return (await fse.write(fd, buf, off, len)).bytesWritten;
 };
