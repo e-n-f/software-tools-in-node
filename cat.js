17945e4f3d08d31e26722a64dd857f4c202d0a91
Eric Fischer <enf@pobox.com>
2017-09-18 17:41:51 -0700

    Trying to present a stdio-type interface

--- cat.js
+++ cat.js
@@ -2,26 +2,26 @@
 
 const io = require('./io.js');
 
-async function cat(fd) {
+async function cat(fp) {
 	var b = Buffer.alloc(2000);
 
 	var n;
-	while ((n = await io.read(fd, b, 0, b.length)) > 0) {
+	while ((n = await fp.fread(b, 0, b.length)) > 0) {
 		var off = 0;
 		while (off < n) {
-			off += await io.write(1, b, off, n - off);
+			off += await io.stdout.fwrite(b, off, n - off);
 		}
 	}
 }
 
 async function main() {
 	if (process.argv.length <= 2) {
-		await cat(0);
+		await cat(io.stdin);
 	} else {
 		for (var i = 2; i < process.argv.length; i++) {
-			var fd = await io.open(process.argv[i], 'r');
-			await cat(fd);
-			await io.close(fd);
+			var fp = await io.fopen(process.argv[i], 'r');
+			await cat(fp);
+			await fp.fclose();
 		}
 	}
 }
--- io.js
+++ io.js
@@ -17,3 +17,28 @@ module.exports.read = async function(fd, buf, off, len) {
 module.exports.write = async function(fd, buf, off, len) {
 	return (await fse.write(fd, buf, off, len)).bytesWritten;
 };
+
+module.exports.FILE = function(thefd) {
+	this.fd = thefd,
+
+	this.fread = async function(buf, off, len) {
+		return await module.exports.read(this.fd, buf, off, len);
+	};
+
+	this.fwrite = async function(buf, off, len) {
+		return await module.exports.write(this.fd, buf, off, len);
+	};
+
+	this.fclose = async function() {
+		return await module.exports.close(this.fd);
+	};
+};
+
+module.exports.stdin = new module.exports.FILE(0);
+module.exports.stdout = new module.exports.FILE(1);
+module.exports.stderr = new module.exports.FILE(2);
+
+module.exports.fopen = async function(name, mode) {
+	var fd = await module.exports.open(name, mode);
+	return new module.exports.FILE(fd);
+}
