2d9df2c2f242bb298fe5a12e5ed31f674ab539eb
Eric Fischer <enf@pobox.com>
2017-09-19 09:53:45 -0700

    Let, not var

--- cat.js
+++ cat.js
@@ -3,11 +3,11 @@
 const io = require('./io.js');
 
 async function cat(fd) {
-	var b = Buffer.alloc(2000);
+	let b = Buffer.alloc(2000);
 
-	var n;
+	let n;
 	while ((n = await io.read(fd, b, 0, b.length)) > 0) {
-		var off = 0;
+		let off = 0;
 		while (off < n) {
 			off += await io.write(1, b, off, n - off);
 		}
@@ -18,8 +18,8 @@ async function main() {
 	if (process.argv.length <= 2) {
 		await cat(0);
 	} else {
-		for (var i = 2; i < process.argv.length; i++) {
-			var fd = await io.open(process.argv[i], 'r');
+		for (let i = 2; i < process.argv.length; i++) {
+			let fd = await io.open(process.argv[i], 'r');
 			await cat(fd);
 			await io.close(fd);
 		}
