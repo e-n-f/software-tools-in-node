9ca092a41fb48475d06e109029468c379b53f155
Eric Fischer <enf@pobox.com>
2017-10-09 22:27:50 -0700

    Use UTF-32 to serialize numbers instead of custom code

--- 008_compress.js
+++ 008_compress.js
@@ -9,34 +9,19 @@ const unixio = require("unixio");
 const THRESH = 5;
 const PREFIX = 160;
 
-async function write_varint(n) {
-	while (n > 0x7f) {
-		await unixio.stdout.putb((n & 0x7f) | 0x80);
-		n >>= 7;
-	}
-	await unixio.stdout.putb(n);
-}
-
-// XXX should be in the library
-async function peekb() {
-	let b = await unixio.stdin.getb();
-	await unixio.stdin.ungetb(b);
-	return b;
-}
-
 async function main() {
 	let b;
 	while ((b = await unixio.stdin.getb()) != unixio.EOF) {
 		let count = 1;
 
-		while (b == (await peekb())) {
+		while (b == (await unixio.stdin.peekb())) {
 			await unixio.stdin.getb();
 			count++;
 		}
 
 		if (count >= THRESH || b == PREFIX) {
 			await unixio.stdout.putb(PREFIX);
-			await write_varint(count);
+			await unixio.stdout.putu(count);
 			await unixio.stdout.putb(b);
 		} else {
 			let i;
--- 009_uncompress.js
+++ 009_uncompress.js
@@ -8,29 +8,11 @@ const unixio = require("unixio");
 
 const PREFIX = 160;
 
-async function read_varint(n) {
-	let count = 0;
-	let shift = 0;
-
-	while (true) {
-		let b = await unixio.stdin.getb();
-
-		count |= (b & 0x7f) << shift;
-		shift += 7;
-
-		if ((b & 0x80) == 0) {
-			break;
-		}
-	}
-
-	return count;
-}
-
 async function main() {
 	let b;
 	while ((b = await unixio.stdin.getb()) != unixio.EOF) {
 		if (b == PREFIX) {
-			let count = await read_varint();
+			let count = await unixio.stdin.getu();
 			b = await unixio.stdin.getb();
 
 			let i;
